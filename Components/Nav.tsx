28934e763ffe44bdee547ffb38fd713564dae7db
Guillermo Trejo <maynardtrejo@yahoo.com>
2025-10-15 22:47:05 -0500

    fixed links in the lg screens

--- Components/Nav.tsx
+++ Components/Nav.tsx
@@ -33,31 +33,44 @@ export const Nav = () => {
         <RxHamburgerMenu />
       </button>
       {isModalOpen && (
-        <div className="fixed inset-0 bg-black z-40 flex items-center justify-center" onClick={handleClose}>
-          <button className="absolute text-5xl top-10 right-4" onClick={handleClose}><IoMdClose /></button>
+        <div
+          className="fixed inset-0 bg-black z-40 flex items-center justify-center"
+          onClick={handleClose}
+        >
+          <button
+            className="absolute text-5xl top-10 right-4"
+            onClick={handleClose}
+          >
+            <IoMdClose />
+          </button>
           <ul
             className={`flex flex-col gap-10 md:gap-4 font-bold text-5xl md:text-md space-x-4 items-center lg:flex-row ${
               isModalOpen ? "flex" : "hidden"
             } md:flex`}
           >
             <li className="hover:scale-150 hover:text-green-500 transition-all duration-300 ease-out">
-              <Link href="#projects">
-                Projects
-              </Link>
+              <Link href="#projects">Projects</Link>
             </li>
             <li className="hover:scale-150 hover:text-green-500 transition-all duration-300 ease-out">
-              <Link href="#tech">
-                Tech
-              </Link>
+              <Link href="#tech">Tech</Link>
             </li>
             <li className="hover:scale-150 hover:text-white hover:animate-pulse transition-all duration-400 ease-out bg-green-500 px-4 py-2 rounded-full cursor-pointer">
-              <Link href="/">
-                Contact
-              </Link>
+              <Link href="/">Contact</Link>
             </li>
           </ul>
         </div>
       )}
+      <ul className="hidden md:flex flex-col gap-10 md:gap-4 font-bold md:text-md space-x-4 items-center lg:flex-row">
+        <li className="hover:scale-150 hover:text-green-500 transition-all duration-300 ease-out">
+          <Link href="#projects">Projects</Link>
+        </li>
+        <li className="hover:scale-150 hover:text-green-500 transition-all duration-300 ease-out">
+          <Link href="#tech">Tech</Link>
+        </li>
+        <li className="hover:scale-150 hover:text-white hover:animate-pulse transition-all duration-400 ease-out bg-green-500 px-4 py-2 rounded-full cursor-pointer">
+          <Link href="/">Contact</Link>
+        </li>
+      </ul>
     </nav>
   );
 };
